856482d8aba4b7a9328aca163264d70c64c31297
JustGritt <alexistan9712@gmail.com>
2024-01-05 11:58:45 +0100

    👻 Minor front fixes

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -15,27 +15,25 @@ export default function Home() {
   return (
     <main className="min-h-screen ">
       <section className='h-11/12 w-full relative min-h-[500px] max-h-[600px] overflow-hidden	'>
-        <img
-          src='https://images.unsplash.com/photo-1560785218-893cc779709b?q=80&w=2792&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt={'Bg'}
-          className='h-[600px] w-full object-cover '
-        />
+        <img src='https://images.unsplash.com/photo-1560785218-893cc779709b?q=80&w=2792&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt={'Bg'}
+          className='h-[600px] w-full object-cover'/>
         <div className="absolute bottom-0 p-6 lg:p-12 w-full">
           <h1 className='text-xl font-inter font-thin text-white '>Explore Our Collaborators</h1>
           <h2 className='text-3xl text-white font-bold'>The Gateway to Exceptional Professional Services</h2>
 
-          <div className="rounded-xl items-left lg:items-center bg-white px-4 py-2 mt-5 flex max-w-[900px] gap-x-4 lg:flex-row flex-col">
+          <div className="rounded-xl items-left lg:items-center bg-white px-4 py-2 mt-5 flex max-w-[900px] justify-around lg:flex-row flex-col">
             <div className="flex-col flex">
-              <label htmlFor="search-service" className="text-gray-500 text-sm">Que cherchez vous ?</label>
-              <input id="search-service" type="text" placeholder='Search for a service' className='rounded-lg text-black focus:outline-none placeholder-black  border lg:border-0 p-2 lg:p-0 w-full lg:w-96' />
+              <label htmlFor="search-service" className="text-gray-500 text-sm mt-1">What are you looking for?</label>
+              <input id="search-service" type="text" placeholder='Cameraman, Photoshoot ...' className='border-0 focus:ring-0 text-black placeholder-black border-b-2 border-b-transparent focus:border-b-slate-600 mt-1 p-2 pl-0 lg:p-0 w-full lg:w-96 transition-colors' />
             </div>
 
-            <div className="flex-col flex">
-              <label htmlFor="search-service" className="text-gray-500 text-sm">Où ?</label>
-              <input id="search-service" type="text" placeholder='Ex: Paris, France 75006' className='rounded-lg text-black focus:outline-none placeholder-black  border lg:border-0 p-2 lg:p-0 w-full lg:w-96' />
+            <div className="flex-col flex mt-2 lg:mt-0">
+              <label htmlFor="search-service" className="text-gray-500 text-sm mt-1">Location</label>
+              <input id="search-service" type="text" placeholder='Paris, France 75006' className='border-0 focus:ring-0 text-black placeholder-black border-b-2 border-b-transparent focus:border-b-slate-600 mt-1 p-2 pl-0 lg:p-0 w-full lg:w-96 transition-colors' />
             </div>
 
-            <div className="mt-2">
-              <Button classNames="w-32 lg:w-auto">
+            <div>
+              <Button classNames="w-full my-2 py-2 lg:w-auto lg:h-full">
                 <FontAwesomeIcon icon={faSearch} />
               </Button>
             </div>
@@ -48,7 +46,7 @@ export default function Home() {
         <div className="flex-1 justify-center items-center flex flex-col p-8">
           <h3 className="text-3xl text-bold font-inter">Are you a professional studio ?</h3>
           <p className="text-gray-500">Discover the ease of online appointment with Odicylens.</p>
-          <Button title="Add Your Compagny" classNames="mt-4" />
+          <Button title="Become a Odicylens affiliate" classNames="mt-4" />
         </div>
 
         <div className="flex-1 p-4 flex-wrap flex-row flex gap-x-4 gap-y-4 justify-center items-center">
@@ -77,9 +75,7 @@ export default function Home() {
       </section>
 
       <section className="flex flex-col lg:flex-row min-h-[600px] py-8  overflow-hidden justify-center items-center bg-[#FAFAFA]">
-        <Faq classNames={{
-          container: 'w-full lg:w-1/2 h-fit'
-        }} />
+        <Faq classNames={{ container: 'w-full lg:w-1/2 h-fit' }} />
       </section>
     </main>
   )
