3ce709acdd451fc1659e921fa72317e9f1704853
JustGritt <alexistan9712@gmail.com>
2024-02-05 20:14:24 +0100

    Update Terms & Footer

--- front/src/app/(home)/terms/page.tsx
+++ front/src/app/(home)/terms/page.tsx
@@ -18,8 +18,8 @@ export default function Terms() {
                     </p>
 
                     <p className="text-gray-500 dark:text-gray-400 block mt-4">
-                        These Terms of Use described the legally binding terms and conditions that oversee your use of the Site. <span className="font-medium">BY LOGGING INTO THE SITE, YOU ARE BEING COMPLIANT THAT THESE TERMS</span> and you represent that you have the authority and capacity to enter into these Terms.
-                        <span className="font-medium">YOU SHOULD BE AT LEAST 18 YEARS OF AGE TO ACCESS THE SITE. IF YOU DISAGREE WITH ALL OF THE PROVISION OF THESE TERMS, DO NOT LOG INTO AND/OR USE THE SITE.</span>
+                        These Terms of Use described the legally binding terms and conditions that oversee your use of the Site.
+                        By accessing or using the Site, you are accepting these Terms (on behalf of yourself or the entity that you represent), and you represent and warrant that you have the right, authority, and capacity to enter into these Terms (on behalf of yourself or the entity that you represent).
                     </p>
 
                     <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2 mt-8">
@@ -29,58 +29,67 @@ export default function Terms() {
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
                                     Subject to these Terms
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, non-commercial use.
+                                </p>
                             </div>
                             <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    Cookies
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">We employ the use of cookies. By accessing Odicylens, you agreed to use cookies in agreement with the Odicylens&apos;s Privacy Policy.</p>
+                                <p className="text-gray-500 dark:text-gray-400 mt-4">
+                                    For the most part, cookies are used by ourselves and our affiliates, we eat them and they are delicious. We are not using them to track your usage of our website and to understand your preferences. If you like cookies, you can eat them too.
+                                </p>
                             </div>
                             <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    License
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">Unless otherwise stated, Odicylens and/or its licensors own the intellectual property rights for all material on Odicylens. All intellectual property rights are reserved. You may access this from Odicylens for your own personal use subjected to restrictions set in these terms and conditions. But since this is a school project, you can do whatever you want with it.</p>
                             </div>
                             <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    Hyperlinking to our Content
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    We are not responsible for any content that appears on your website. You agree to protect and defend us against all claims that is rising on your website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
+                                </p>
                             </div>
                         </div>
                         <div>
                         <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    Content Liability
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
+                                </p>
                             </div>
                             <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    Removal of links from our website
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.
+                                </p>
+                                <p className="text-gray-500 dark:text-gray-400 mt-4">
+                                    We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+                                </p>
                             </div>
                             <div className="mb-10">
                                 <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
                                     <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
+                                    Disclaimer
                                 </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
-                            </div>
-                            <div className="mb-10">
-                                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-                                    <QuestionMarkCircleIcon className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" />
-                                    Subject to these Terms
-                                </h3>
-                                <p className="text-gray-500 dark:text-gray-400">Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use</p>
+                                <p className="text-gray-500 dark:text-gray-400">
+                                    This is not a real website, it is a school project. We are not responsible for any content that appears on your website. You agree to protect and defend us against all claims that is rising on your website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights. Also if you like cookies you should eat them, if you are reading this, we would like to thank you for reading this far and give you a cookie.
+                                </p>
                             </div>
                         </div>
                     </div>
