d981f0dca7b0077e69d2d17124025e07a3859f51
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-02-08 22:54:14 +0100

    done

--- front/src/app/affiliate/page.tsx
+++ front/src/app/affiliate/page.tsx
@@ -35,46 +35,7 @@ export default function Affiliate() {
         affiliate(values)
             .unwrap()
             .then((res) => {
-                router.push("login");
-                toast.custom((t) => (
-                    <div
-                        className={`${"animate-enter"} max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-                    >
-                        <div className="flex-1 w-0 p-4">
-                            <div className="flex items-start">
-                                <div className="ml-3 flex-1">
-                                    <p className="text-sm font-medium text-gray-900">
-                                        Becoming Member
-                                    </p>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                        Your submittion has been taken into account, our moderation team will
-                                        handle your request shortly.
-                                        <br />
-                                        <br />
-                                        If you encounter any issues or have questions, feel free to
-                                        contact our support team at :
-                                        <a
-                                            className="text-main hover:text-main-dark transition-all"
-                                            href="mailto:support@example.com"
-                                        >
-                                            support@example.com
-                                        </a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex border-l border-gray-200">
-                            <button
-                                onClick={() => {
-                                    toast.dismiss(t.id);
-                                }}
-                                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                            >
-                                Close
-                            </button>
-                        </div>
-                    </div>
-                ));
+                router.push("/confirm-member");
             })
             .catch((affiliateError) => {
                 toast.error((affiliateError as ApiErrorResponse).data["detail"])
