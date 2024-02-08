e501e0c538bdd8be4cd8124ca02194a6469572ea
Alexis TAN <alexis.tan@raja.fr>
2024-02-08 16:13:18 +0100

    Remove unused company fields

--- front/src/app/dashboard/company/page.tsx
+++ front/src/app/dashboard/company/page.tsx
@@ -24,12 +24,6 @@ export default function Company() {
                                     {user?.companie.name}
                                     <span className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Latest</span>
                                 </h3>
-                                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                                    {user?.companie.created_at}
-                                </time>
-                                <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                                    {user?.companie.description}
-                                </p>
                                 <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700 mt-2">
                                     <ShoppingBagIcon className="w-5 h-5 me-2" aria-hidden="true" />
                                     Link to Store
