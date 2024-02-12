0dfcbba52ef0165ee0189b1a35b7c6a55f6d9a43
Alexis TAN <alexistan9712@gmail.com>
2024-02-12 16:59:22 +0100

    Start Calendar

--- front/src/app/dashboard/appointments/page.tsx
+++ front/src/app/dashboard/appointments/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useState, useEffect } from 'react';
-import BookingCalendar from '@/components/Calendar/BookingCalendar';
-import { useSelector } from "react-redux";
-import { selectCurrentUser } from "@/lib/services/slices/authSlice";
 
+
+import BookingCalendar from '@/components/Calendar/BookingCalendar';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction';
@@ -11,22 +9,54 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import { string } from 'yup';
 import { Button } from '@/components/Ui/Button';
 
-export default function Page() {
+import Link from 'next/link';
+import Breadcrumb from '@/components/Header/Breadcrumb';
+import { Company } from "@/types/Company";
+import { Employee } from "@/types/User";
+import { useSelector } from 'react-redux';
+import { useEffect, useMemo, useState } from 'react';
+import { selectCurrentUser, selectCurrentUserConfig } from '@/lib/services/slices/authSlice';
+import { CheckIcon, IdentificationIcon, UserIcon, HomeModernIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { getUserCookie } from "@/lib/helpers/UserHelper";
+import { UserCookieType } from "@/types/User";
+import toast from "react-hot-toast";
 
-    const user = useSelector(selectCurrentUser);
+export default function Page() {
+    const data = [];
     const [scheduleFetched, setScheduleFetched] = useState(false);
     const [schedules, setSchedules] = useState([]);
+
+    const user = useSelector(selectCurrentUser);
+    const userConfig: { [key: string]: boolean } = useSelector(selectCurrentUserConfig);
+    const userRoles = useMemo(() => Object.keys(userConfig || {}).filter(role => userConfig[role]), [userConfig]);
+
+    // Get session
+    const [parsedSession, setParsedSession] = useState<any>({});
+    useEffect(() => {
+        (async () => {
+            const session = await getUserCookie(UserCookieType.SESSION);
+            const parsedSession = JSON.parse(session?.value || "{}");
+            setParsedSession(parsedSession);
+        })();
+    }, [])
 
     useEffect(() => {
         if (user && !scheduleFetched) {
             fetch(`https://api.odicylens.com/users/${user?.id}/schedules`, { method: "GET" })
                 .then((res) => res.json())
-                .then((data) => { console.log(data) });
+                .then((data) => {
+                    const events = data["hydra:member"].map((event: any) => ({
+                        title: event.onVacation ? "Vacation" : "Work",
+                        start: event.startDate,
+                        end: event.endDate
+                    }));
+
+                    console.log(events);
+                    // setSchedules(events);
+                })
             setScheduleFetched(true);
         }
     }, [scheduleFetched, user]);
-
-    const data = [] as any;
 
     const handleDateSelect = (arg: any) => {
         const eventCategory = document.getElementById('selectedEvent') as HTMLSelectElement;
@@ -44,7 +74,7 @@ export default function Page() {
 
             data.push({
                 title,
-                // Replace 024-02-13T07:00:00.000Z to 2024-02-15 10:00:00
+                // Replace 2024-02-13T07:00:00.000Z to 2024-02-15 10:00:00
                 start: arg.start.toISOString().replace('T', ' ').replace('Z', ''),
                 end: arg.end.toISOString().replace('T', ' ').replace('Z', ''),
             });
@@ -76,47 +106,36 @@ export default function Page() {
         <section className="lg:pl-72 block min-h-screen">
             <div className="p-4 sm:p-6 lg:p-8 h-full">
                 <div className="mx-auto bg-white dark:bg-slate-800 px-8 py-8 rounded-xl shadow">
-                    <div className="lg:flex lg:h-full lg:flex-col">
-                        {/* <FullCalendar
-                            plugins={[ timeGridPlugin, interactionPlugin ]}
-                            initialView="timeGridWeek"
-                            headerToolbar={{
-                                left: 'prev,next today',
-                                center: 'title',
-                                right: 'timeGridWeek,timeGridDay'
-                            }}
-                            expandRows={true}
-                            selectable={true}
-                            select={handleDateSelect}
-                            eventClick={handleRemoveEvent}
-                            events={data}
+                    {
+                        (userRoles.includes("isOwner") || userRoles.includes("isWorker")) && (
+                            <div className="lg:flex lg:h-full lg:flex-col">
+                                <div className="flex justify-center items-center mb-8">
+                                    <select id="selectedEvent" className="flex-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option defaultValue="work">Work</option>
+                                        <option defaultValue="vacation">Vacation</option>
+                                    </select>
 
-                        /> */}
+                                    <Button onClick={() => saveEvents()} className="flex-none flex-2 ml-4">
+                                        Save events
+                                    </Button>
+                                </div>
 
-                        <div className="flex justify-center items-center mb-8">
-                            <select id="selectedEvent" className="flex-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                <option defaultValue="work">Work</option>
-                                <option defaultValue="vacation">Vacation</option>
-                            </select>
-
-                            <Button onClick={() => saveEvents()} className="flex-none flex-2 ml-4">
-                                Save events
-                            </Button>
-                        </div>
-
-                        <FullCalendar
-                            plugins={[ timeGridPlugin, interactionPlugin ]}
-                            initialView="timeGridWeek"
-                            nowIndicator={true}
-                            editable={true}
-                            selectable={true}
-                            selectMirror={true}
-                            selectOverlap={false}
-                            select={handleDateSelect}
-                            eventClick={handleRemoveEvent}
-                            events={data}
-                        />
-                    </div>
+                                <FullCalendar
+                                    plugins={[ timeGridPlugin, interactionPlugin ]}
+                                    initialView="timeGridWeek"
+                                    editable={true}
+                                    selectable={true}
+                                    nowIndicator={true}
+                                    selectMirror={true}
+                                    selectOverlap={true}
+                                    eventOverlap={false}
+                                    events={schedules}
+                                    select={handleDateSelect}
+                                    eventClick={handleRemoveEvent}
+                                />
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </section>
