4ed6dca50356f50994ae40488aca02287f5ed5cb
WalidKhalqallah <wkhalqallah2@myges.fr>
2024-02-16 11:30:54 +0100

    new employee form

--- front/src/app/dashboard/stores/[storeId]/employees/add/page.tsx
+++ front/src/app/dashboard/stores/[storeId]/employees/add/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NewEmployee from "@/components/Forms/NewEmployee";
 import Breadcrumb from "@/components/Header/Breadcrumb";
 
 export default function StoreNewEmployee() {
@@ -7,9 +8,7 @@ export default function StoreNewEmployee() {
         <section className="lg:pl-72 block min-h-screen">
             <div className="p-4 sm:p-6 lg:p-8 h-full">
                 <Breadcrumb />
-                <div className="mt-4 mx-auto bg-white dark:bg-slate-800 px-8 py-8 rounded-xl shadow border">
-                    Add new employee
-                </div>
+                <NewEmployee />
             </div>
         </section>
     )
